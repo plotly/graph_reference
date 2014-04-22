7a01eabfc9fe516dffc5c981e5e725d878319f75
Chris P <christopher.parmer@gmail.com>
2014-04-22 13:43:17 -0400

    added opacity to histograms

--- graph_objs_meta.py
+++ graph_objs_meta.py
@@ -796,6 +796,8 @@ INFO = OrderedDict([
             type='object',
             val_types=val_types['general']['object'])),
 
+        ('opacity', drop_in['opacity']),
+
         ('line', dict(
             required=False,
             type='object',
@@ -860,6 +862,8 @@ INFO = OrderedDict([
             required=False,
             type='object',
             val_types=val_types['general']['object'])),
+
+        ('opacity', drop_in['opacity']),
 
         ('autobiny', dict(
             required=False,
